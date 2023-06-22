67792e6d72d07fa22ab94ebb6b28ec5934d8f3f3
Polina Ryabova <polinaryabova.2@gmail.com>
2023-06-23 01:44:28 +0500

    add header layout

--- React-app/src/App.js
+++ React-app/src/App.js
@@ -2,7 +2,35 @@ import React from 'react';
 import logo from './assets/img/logo.png';
 
 function App() {
-  return <div>Hello World</div>;
+      <header className="header">
+        <div className="header__container container">
+          <div className="header__logo logo">
+            <img src={logo} alt="логотип" className="logo__img" />
+            <p className="logo__text">Simpl. Система рекрутинга</p>
+          </div>
+          <nav className="header__nav">
+            <ul className="nav__list">
+              <li className="nav__item nav__item--active">
+                <a href="#" className="nav__link">
+                  Все резюме
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  Загрузить резюме
+                </a>
+              </li>
+
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  Мой профиль
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
 }
 
 export default App;
