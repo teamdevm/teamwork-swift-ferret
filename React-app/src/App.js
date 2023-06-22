0a2fa67805d48670ce06e40fa5e5f0b1b07ba305
Polina Ryabova <polinaryabova.2@gmail.com>
2023-06-23 01:46:30 +0500

    add settings layout

--- React-app/src/App.js
+++ React-app/src/App.js
@@ -31,6 +31,65 @@ function App() {
         </div>
       </header>
 
+          <section className="main__settings">
+            <div className="main__find-count">Найдено: 13</div>
+            <div className="main__select">
+              <label className="main__label">
+                <input
+                  type="checkbox"
+                  className="main__input input-checkbox"
+                  name="select all"
+                  value="all"
+                />
+                <span>Выбрать всех</span>
+              </label>
+              <button className="main__btn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-file-input"
+                >
+                  <path d="M4 22h14a2 2 0 0 0 2-2V7.5L14.5 2H6a2 2 0 0 0-2 2v4" />
+                  <polyline points="14 2 14 8 20 8" />
+                  <path d="M2 15h10" />
+                  <path d="m9 18 3-3-3-3" />
+                </svg>
+                Выгрузить в Exel
+              </button>
+            </div>
+            <div className="main__sort sort">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-chevron-down"
+              >
+                <polyline points="6 9 12 15 18 9" />
+              </svg>
+              <p className="sort__title">
+                Сортировать по: <span>ФИО</span>
+              </p>
+              <ul className="sort__list sort__list--hidden">
+                <li className="sort__item sort__item--active">ФИО</li>
+                <li className="sort__item">статусу</li>
+                <li className="sort__item">вакансии</li>
+                <li className="sort__item">дате добавления</li>
+              </ul>
+            </div>
+          </section>
 }
 
 export default App;
