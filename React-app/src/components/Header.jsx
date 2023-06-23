cd5a4a46d0fad751a373a65f147ebd6d62442141
Polina Ryabova <polinaryabova.2@gmail.com>
2023-06-23 16:43:30 +0500

    add base struct to header component

---
+++ React-app/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from '../assets/img/logo.png';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__container container">
+        <div className="header__logo logo">
+          <img src={logo} alt="логотип" className="logo__img" />
+          <p className="logo__text">Simpl. Система рекрутинга</p>
+        </div>
+        <nav className="header__nav">
+          <ul className="nav__list">
+            <Link to="/">
+              <li className="nav__item nav__item--active">Все резюме</li>
+            </Link>
+            <Link to="/download">
+              <li className="nav__item">Загрузить резюме</li>
+            </Link>
+            <Link to="/sign-in">
+              <li className="nav__item">Вход</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
