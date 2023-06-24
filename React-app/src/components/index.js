cc28dbeb5fbd92e5af50f9aea7f8540f9222e4d2
Polina Ryabova <polinaryabova.2@gmail.com>
2023-06-24 23:20:19 +0500

    add status popup component

--- React-app/src/components/index.js
+++ React-app/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Filters } from './Filters';
 export { default as FilterCheckbox } from './FilterCheckbox';
 export { default as FilterText } from './FilterText';
 export { default as SortPopup } from './SortPopup';
+export { default as StatusPopup } from './StatusPopup';
