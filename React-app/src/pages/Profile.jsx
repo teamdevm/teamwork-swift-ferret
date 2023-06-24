5b9bfd1b9d18fc3a9f013a60325467f316f0f111
Polina Ryabova <polinaryabova.2@gmail.com>
2023-06-24 17:55:26 +0500

    add social icon's files

---
+++ React-app/src/pages/Profile.jsx
@@ -0,0 +1,5 @@
+import github from '../assets/img/github.png';
+import linkedin from '../assets/img/linkedin.png';
+import telegram from '../assets/img/telegram.png';
+import viber from '../assets/img/viber.png';
+import whatsapp from '../assets/img/whatsapp.png';
