744e7273a5787b08756d06067c3f0f7854e71630
Polina Ryabova <polinaryabova.2@gmail.com>
2023-06-24 23:00:34 +0500

    export all new components

--- React-app/src/components/index.js
+++ React-app/src/components/index.js
@@ -1,1 +1,5 @@
 export { default as Header } from './Header';
+export { default as Filters } from './Filters';
+export { default as FilterCheckbox } from './FilterCheckbox';
+export { default as FilterText } from './FilterText';
+export { default as SortPopup } from './SortPopup';
