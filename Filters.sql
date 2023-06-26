b0bb7625799e5a474594783073d7e3aefb5e6cd8
oborinaek <134125791+oborinaek@users.noreply.github.com>
2023-06-26 15:10:48 +0500

    Тестовые данные и запросы для фильтрации

---
+++ Filters.sql
@@ -0,0 +1,10 @@
+--Для фильтра города
+SELECT DISTINCT Loc FROM Personal_Information;
+
+--Для фильтра университет
+SELECT DISTINCT University FROM Education;
+
+--Для фильтра статус
+SELECT DISTINCT status FROM Status;
+
+SELECT * FROM *;
